e7c104f6fc5fca5fb4096f4166969f40de648412
dannyyholloww <dannyyhollow03@gmail.com>
2024-09-11 19:25:00 -0500

    Laboratorio 5 completado

---
+++ Laboratorio_5/Laboratorio_5.6/Program.cs
@@ -0,0 +1,11 @@
+﻿Dictionary<string, string> paisesYCapitales = new Dictionary<string, string>
+{
+    {"Francia", "Paris" },
+    {"España", "Madrid" },
+    {"Italia", "Roma" }
+};
+
+foreach (KeyValuePair<string, string> par int paisesYCapitales)
+{
+    Console.WriteLine("La capital de " + par.Key + "es" + par.Value + ".");
+}
