7f93217aa0fed2707e56d235f6f735e1dc1ca2cc
dannyyholloww <dannyyhollow03@gmail.com>
2024-09-23 21:00:39 -0500

    Laboratorio 9 completado

---
+++ Laboratorio_9/Laboratorio_9/Aleatorios.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+class Aleatorios
+{
+    private Random random = new Random();
+
+    public int GenerarNumeroEntre(int min, int max)
+    {
+        return random.Next(min, max + 1);
+    }
+
+    public int[] GenerarArregloDeNumeros(int cantidad, int min, int max)
+    {
+        int[] numeros = new int[cantidad];
+        for (int i = 0; i < cantidad; i++)
+        {
+            numeros[i] = random.Next(min, max + 1);
+        }
+        return numeros;
+    }
+
+    public int[] GenerarNumerosNoRepetidos(int cantidad, int min, int max)
+    {
+        if (max - min + 1 < cantidad)
+        {
+            throw new ArgumentException("El rango no es suficiente para generar números no repetidos.");
+        }
+
+        int[] numeros = new int[cantidad];
+        int index = 0;
+
+        while (index < cantidad)
+        {
+            int numero = random.Next(min, max + 1);
+            if (Array.IndexOf(numeros, numero) == -1)
+            {
+                numeros[index] = numero;
+                index++;
+            }
+        }
+
+        return numeros;
+    }
+}
---
+++ Laboratorio_9/Laboratorio_9/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Write("Ingrese el lado 1 del triángulo: ");
+        int lado1 = int.Parse(Console.ReadLine());
+
+        Console.Write("Ingrese el lado 2 del triángulo: ");
+        int lado2 = int.Parse(Console.ReadLine());
+
+        Console.Write("Ingrese el lado 3 del triángulo: ");
+        int lado3 = int.Parse(Console.ReadLine());
+
+        if ((lado1 + lado2 > lado3) && (lado1 + lado3 > lado2) && (lado2 + lado3 > lado1))
+        {
+            if (lado1 == lado2 && lado2 == lado3)
+            {
+                Console.WriteLine("El triángulo es equilátero.");
+            }
+            else if (lado1 == lado2 || lado2 == lado3 || lado1 == lado3)
+            {
+                Console.WriteLine("El triángulo es isósceles.");
+            }
+            else
+            {
+                Console.WriteLine("El triángulo es escaleno.");
+            }
+        }
+        else
+        {
+            Console.WriteLine("No es un triángulo válido.");
+        }
+    }
+}
+
+
