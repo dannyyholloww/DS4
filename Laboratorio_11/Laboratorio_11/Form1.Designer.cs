09ba7eb6bf95e8a4dff01f976818e763379db28c
dannyyholloww <dannyyhollow03@gmail.com>
2024-10-02 23:10:30 -0500

    Laboratorio 11 completado

---
+++ Laboratorio_11/Laboratorio_11/Form1.Designer.cs
@@ -0,0 +1,74 @@
+﻿namespace Laboratorio_11
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btn_ClickThis = new System.Windows.Forms.Button();
+            this.lblHelloWorld = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btn_ClickThis
+            // 
+            this.btn_ClickThis.Location = new System.Drawing.Point(181, 125);
+            this.btn_ClickThis.Name = "btn_ClickThis";
+            this.btn_ClickThis.Size = new System.Drawing.Size(75, 23);
+            this.btn_ClickThis.TabIndex = 0;
+            this.btn_ClickThis.Text = "Click this";
+            this.btn_ClickThis.UseVisualStyleBackColor = true;
+            this.btn_ClickThis.Click += new System.EventHandler(this.btn_ClickThis_Click);
+            // 
+            // lblHelloWorld
+            // 
+            this.lblHelloWorld.AutoSize = true;
+            this.lblHelloWorld.Location = new System.Drawing.Point(202, 151);
+            this.lblHelloWorld.Name = "lblHelloWorld";
+            this.lblHelloWorld.Size = new System.Drawing.Size(35, 13);
+            this.lblHelloWorld.TabIndex = 1;
+            this.lblHelloWorld.Text = "label1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lblHelloWorld);
+            this.Controls.Add(this.btn_ClickThis);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btn_ClickThis;
+        private System.Windows.Forms.Label lblHelloWorld;
+    }
+}
+
---
+++ Parcial_1/Parcial_1/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+
+        Console.WriteLine("Por favor escriba  un numero (Debe ser impar): ");
+        int N = int.Parse(Console.ReadLine());
+
+
+        if (N % 2 == 0)
+        {
+            Console.WriteLine("Incorrecto,el numero que escribio es par");
+        }
+
+        int[,] matriz = new int[N, N];
+        Random random = new Random();
+
+        
+        for (int i = 0; i < N; i++)
+        {
+            matriz[i, i] = random.Next(1, 101); 
+            matriz[i, N - 1 - i] = random.Next(1, 101); 
+        }
+
+      
+        Console.WriteLine("\nMatriz:");
+        for (int i = 0; i < N; i++)
+        {
+            for (int j = 0; j < N; j++)
+            {
+                Console.Write(matriz[i, j] + "\t");
+            }
+            Console.WriteLine();
+        }
+
+        
+        int sumaDeLaDiagonaPrincipal = 0;
+        int sumaDeLaDiagonalInversa = 0;
+
+        for (int i = 0; i < N; i++)
+        {
+            sumaDeLaDiagonaPrincipal += matriz[i, i];
+            sumaDeLaDiagonalInversa += matriz[i, N - 1 - i];
+        }
+
+
+        Console.WriteLine("\nSuma de la diagonal principal: " + sumaDeLaDiagonaPrincipal);
+        Console.WriteLine("\nSuma de la diagonal inversa: " + sumaDeLaDiagonalInversa);
+    }
+}
